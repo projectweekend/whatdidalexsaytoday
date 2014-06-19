412dc1084668cadae931c09f4a1efec8827f40b8
Scott Ferguson <scottwferg@gmail.com>
2014-06-19 11:26:08 -0500

    DAMMIT BRIAN

--- hotdog.go
+++ hotdog.go
@@ -18,7 +18,7 @@ var (
 		"Hey, fuck egg shells. If one of those little fuckers falls in your scrambled eggs it's almost impossible to fish it out.",
 		"My evenings are pretty booked up",
 		"Follow my live tweets bro?",
-		"I've got to make my kilt payment"
+		"I've got to make my kilt payment",
 	}
 )
 
